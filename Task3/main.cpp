fa16f2177cb88732e56ce67d68a1e4ed5816d9cf
Trifle Urgot <trurgot@oa-o3.msk.21-school.ru>
2022-09-08 14:53:36 +0300

    3-parcing done

--- Task3/main.cpp
+++ Task3/main.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <ctime>
 #include <sstream>
+// #include <nlohmann/json.hpp>
 
 struct Product
 {
@@ -28,19 +29,73 @@ void fillTm(std::string value, tm & data)
 
 void tokenize(std::string prod_inpt, std::vector<std::string> & tmp)
 {
+	std::string::iterator beginP;
+	std::string::iterator endP;
+	std::string	str;
 	for (std::string::iterator iter = prod_inpt.begin(); iter < prod_inpt.end(); iter++)
 	{
-		
+		str = "";
+		while (((*iter) == 123 || (*iter) == 32 || (*iter) == 91 || (*iter) == 44 || (*iter) == 93) && iter < prod_inpt.end())
+			iter++;
+		beginP = iter;
+		while ((*iter) != 125 && iter < prod_inpt.end())
+			iter++;
+		endP = iter;
+		copy(beginP, endP, inserter(str, str.begin()));
+		// std::cout << str << std::endl;
+		if (str != "")
+			tmp.push_back(str);
 	}
-	
-//     size_t start;
-//     size_t end = 0;
- 
-//     while ((start = str.find_first_not_of(delim, end)) != std::string::npos)
-//     {
-//         end = str.find(delim, start);
-//         out.push_back(str.substr(start, end - start));
-//     }
+}
+
+void splitTokens(std::string tmp0, std::vector<std::string> & tmp1)
+{
+	std::string::iterator beginP;
+	std::string::iterator endP;
+	std::string	str;
+	for (std::string::iterator iter = tmp0.begin(); iter < tmp0.end(); iter++)
+	{
+		str = "";
+		while (((*iter) == 32 || (*iter) == 44) && iter < tmp0.end())
+			iter++;
+		beginP = iter;
+		while ((*iter) != 44 && iter < tmp0.end())
+			iter++;
+		endP = iter;
+		copy(beginP, endP, inserter(str, str.begin()));
+		// std::cout << str << std::endl;
+		tmp1.push_back(str);
+	}
+}
+
+void splitPairs(std::string tokenStr, std::string & key, std::string & value)
+{
+	std::string::iterator beginP;
+	std::string::iterator endP;
+	std::string	str;
+	for (std::string::iterator iter = tokenStr.begin(); iter < tokenStr.end(); iter++)
+	{
+		str = "";
+		while (((*iter) == 32 || (*iter) == 34) && iter < tokenStr.end())
+			iter++;
+		beginP = iter;
+		while ((*iter) != 34 && (*iter) != 58 && (*iter) != 44 && iter < tokenStr.end())
+			iter++;
+		endP = iter;
+		copy(beginP, endP, inserter(str, str.begin()));
+		// std::cout << str << std::endl;
+		key = str;
+		str = "";
+		while (((*iter) == 32 || (*iter) == 34 || (*iter) == 58) && iter < tokenStr.end())
+			iter++;
+		beginP = iter;
+		while ((*iter) != 34  && (*iter) != 44 && iter < tokenStr.end())
+			iter++;
+		endP = iter;
+		copy(beginP, endP, inserter(str, str.begin()));
+		// std::cout << str << std::endl;
+		value = str;
+	}
 }
 
 // // Компаратор
@@ -59,9 +114,35 @@ void tokenize(std::string prod_inpt, std::vector<std::string> & tmp)
 
 void fillData(std::string prod_inpt, std::vector<Product> & products)
 {
-	std::vector<std::string> tmp;
+	std::vector<std::string> tmp, tmp1;
+	std::string key, value;
+	Product	product;
 	
 	tokenize(prod_inpt, tmp);
+
+	// for (std::vector<std::string>::iterator iter = tmp.begin(); iter < tmp.end(); iter++)
+	// 	std::cout << *iter << std::endl;
+
+	for (std::vector<std::string>::iterator iter = tmp.begin(); iter < tmp.end(); iter++)
+	{
+		splitTokens(*iter, tmp1);
+		// for (std::vector<std::string>::iterator iter = tmp1.begin(); iter < tmp1.end(); iter++)
+		// 	std::cout << *iter << std::endl;
+		for (std::vector<std::string>::iterator iter1 = tmp1.begin(); iter1 < tmp1.end(); iter1++)
+		{
+			splitPairs(*iter1, key, value);
+			// std::cout << "key: " << key << " value: " << value << std::endl;
+			if (key == "id")
+				product.id = stoi(value);
+			if (key == "name")
+				product.name = value;
+			if (key == "price")
+				product.price = stoi(value);
+			if (key == "date")
+				fillTm(value, product.productDate);
+		}
+		products.push_back(product);
+	}
 
 // 	std::vector<std::string> inpt;
 // 	int n, k;
@@ -174,8 +255,18 @@ int main (int argc, char **argv)
 		}
 	}
 
-	fillData(prod_inpt, products);
 	fillFilter(filter_inpt, PRICE_LESS_THAN, DATE_AFTER, NAME_CONTAINS, PRICE_GREATER_THAN, DATE_BEFORE);
+
+	// std::cout << "PRICE_LESS_THAN: " << PRICE_LESS_THAN << std::endl;
+	// std::cout << "DATE_AFTER: " << DATE_AFTER.tm_mday << "." << DATE_AFTER.tm_mon << "." << DATE_AFTER.tm_year << std::endl;
+	// std::cout << "NAME_CONTAINS: " << NAME_CONTAINS << std::endl;
+	// std::cout << "PRICE_GREATER_THAN: " << PRICE_GREATER_THAN << std::endl;
+	// std::cout << "DATE_BEFORE: " << DATE_BEFORE.tm_mday << "." << DATE_BEFORE.tm_mon << "." << DATE_BEFORE.tm_year << std::endl;
+
+	fillData(prod_inpt, products);
+
+	// for (std::vector<Product>::iterator iter = products.begin(); iter < products.end(); iter++)
+	// 	std::cout << "id: " << (*iter).id << ", name: " << (*iter).name << ", price: " << (*iter).price << ", date: " << (*iter).productDate.tm_mday << "." << (*iter).productDate.tm_mon << "." << (*iter).productDate.tm_year << std::endl;
 
 
 	// for (std::vector<Disciplines>::iterator iter = disciplines.begin(); iter < disciplines.end(); iter++)
@@ -183,30 +274,30 @@ int main (int argc, char **argv)
 	// for (std::vector<Participants>::iterator iter = participants.begin(); iter < participants.end(); iter++)
 	// 	std::cout << (*iter).identifier << "  " << (*iter).disciplineName << "  " << (*iter).numberOfTicks << "  " << (*iter).penalty << std::endl;
 
-	for (iterD = disciplines.begin(); iterD < disciplines.end(); iterD++)
-	{
-		participants_tmp.clear();
-		for (iterP = participants.begin(); iterP < participants.end(); iterP++)
-		{
-			if ((*iterD).disciplineName == (*iterP).disciplineName)
-				participants_tmp.push_back(*iterP);
-		}
-		sort (participants_tmp.begin(), participants_tmp.end(), comp);
-		int j = 0;
-		std::vector<Participants>::iterator iter = participants_tmp.begin();
-		while (j < (*iterD).maxQtyParticipants)
-		{
-			j++;
-			outp.push_back(*iter);
-			iter++;
-		}
-	}
+	// for (iterD = disciplines.begin(); iterD < disciplines.end(); iterD++)
+	// {
+	// 	participants_tmp.clear();
+	// 	for (iterP = participants.begin(); iterP < participants.end(); iterP++)
+	// 	{
+	// 		if ((*iterD).disciplineName == (*iterP).disciplineName)
+	// 			participants_tmp.push_back(*iterP);
+	// 	}
+	// 	sort (participants_tmp.begin(), participants_tmp.end(), comp);
+	// 	int j = 0;
+	// 	std::vector<Participants>::iterator iter = participants_tmp.begin();
+	// 	while (j < (*iterD).maxQtyParticipants)
+	// 	{
+	// 		j++;
+	// 		outp.push_back(*iter);
+	// 		iter++;
+	// 	}
+	// }
 
-	sort (outp.begin(), outp.end(), comp2);
+	// sort (outp.begin(), outp.end(), comp2);
 
-	// std::cout << "Output:" << std::endl;
-	for (std::vector<Participants>::iterator iter = outp.begin(); iter < outp.end(); iter++)
-		std::cout << (*iter).identifier << std::endl;
+	// // std::cout << "Output:" << std::endl;
+	// for (std::vector<Participants>::iterator iter = outp.begin(); iter < outp.end(); iter++)
+	// 	std::cout << (*iter).identifier << std::endl;
 
 	return 0;
 }
